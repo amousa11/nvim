5a06a93ab891422f346b95b6ed9e8ef4e2a74ba2
Ali Mousa <alimousa@berkeley.edu>
2024-01-11 22:36:15 -0800

    fix: add more formatters to conform setup

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,22 +1,34 @@
 return {
     'stevearc/conform.nvim',
-    opts = {
-        formatters_by_ft = {
-            lua = { "stylua" },
-            -- Conform will run multiple formatters sequentially
-            python = { "black" },
-            -- Use a sub-list to run only the first available formatter
-            javascript = { { "prettierd", "prettier" } },
-        },
-        format_on_save = {
-            timeout_ms = 5000,
-            lsp_fallback = true
-        }
-    },
-    config = function(opts)
-        require("conform").setup(opts)
-        vim.keymap.set("x", "<Leader>f", function()
-            vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-        end)
+    config = function()
+        local conform = require("conform")
+        conform.setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                -- Conform will run multiple formatters sequentially
+                python = { "black" },
+                -- Use a sub-list to run only the first available formatter
+                javascript = { { "prettierd", "prettier" } },
+                typescript = { { "prettierd", "prettier" } },
+                javascriptreact = { { "prettierd", "prettier" } },
+                typescriptreact = { { "prettierd", "prettier" } },
+                css = { { "prettierd", "prettier" } },
+                html = { { "prettierd", "prettier" } },
+                json = { { "prettierd", "prettier" } },
+                markdown = { { "prettierd", "prettier" } },
+                graphql = { { "prettierd", "prettier" } },
+            },
+            format_on_save = {
+                timeout_ms = 5000,
+                lsp_fallback = true
+            }
+        })
+        vim.keymap.set('x', "<leader>f", function()
+            conform.format({
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 1000,
+            })
+        end, { desc = "Format file or range (in visual mode)" })
     end
 }
