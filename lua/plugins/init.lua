619472a0dd0b9afc94e4ae8ef9dd2eb4341e8432
Ali Mousa <ali.mousa@disneystreaming.com>
2024-02-21 20:01:18 -0800

    feat: fix tree sitter for work setup, switch tokyo night theme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,10 +1,11 @@
 return {
 	{
-		"rose-pine/neovim",
-		name = "rose-pine",
+		"folke/tokyonight.nvim",
+		lazy = false,
 		priority = 1000,
+		opts = {},
 		config = function()
-			vim.cmd("colorscheme rose-pine")
+			vim.cmd("colorscheme tokyonight-night")
 		end,
 	},
 	"nvim-treesitter/playground",
